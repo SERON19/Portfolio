202a050fe52a689aa4e525d0d5b8a71406fb38d4
ADMIN <seroncj@gmail.com>
2024-09-12 14:48:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// var tablinks = document.getElementsByClassName("tablelink");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+
+
+// function opentab(tabname){
+//    for(tablink of tablinks)
+//    {
+//     tablinks.getElementsByClassName
+//    }
+
+    
+// }
