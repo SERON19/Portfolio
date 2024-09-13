59f936be6db7085be6d62fc9f98a77f6b02799b4
ADMIN <seroncj@gmail.com>
2024-09-13 09:14:51 +0300

    Final commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// var tablinks = document.getElementsByClassName("tablelink");
-// var tabcontents = document.getElementsByClassName("tab-contents");
 
+const tablinks = document.getElementsByClassName("tablelink");
+const tabcontents= document.getElementsByClassName("tab-contents")
 
-// function opentab(tabname){
-//    for(tablink of tablinks)
-//    {
-//     tablinks.getElementsByClassName
-//    }
-
-    
-// }
+function opentab(tabname){
+    for(tab of tablinks){
+        tab.classList.remove("active-link");
+    }
+    for(tabcont of tabcontents){
+        tabcont.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-tab"); 
+    document.getElementById(tabname).classList.add("active-tab")
+}
